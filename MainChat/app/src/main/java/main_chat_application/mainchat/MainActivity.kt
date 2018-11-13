bf47350edec1153453c9dddddbabb7a986afee6d
Ratthapon-n <ratthapon@cmu.ac.th>
2018-11-14 02:24:18 +0700

    ChatNo
    Not finish

---
+++ MainChat/app/src/main/java/main_chat_application/mainchat/MainActivity.kt
@@ -0,0 +1,36 @@
+package main_chat_application.mainchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Group Chat"
+        val adapter = GroupAdapter<RecyclerView.ViewHolder>()
+
+        adapter.add(UserItem())
+
+        recyclerview_message.adapter = adapter
+
+    }
+}
+
+class UserItem: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.other_message
+    }
+
+}
