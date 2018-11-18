98434cf1732fd7637985a2cf27ab76ab20da0af8
borirat-k <borirat_khampingyot@cmu.ac.th>
2018-11-18 17:42:04 +0700

    bug json jdownload

---
+++ MainChat/app/src/main/java/main_chat_application/mainchat/JSonDownloader.kt
@@ -0,0 +1,116 @@
+package main_chat_application.mainchat
+
+import android.app.ProgressDialog
+import android.content.Context
+import android.os.AsyncTask
+import android.support.v7.widget.RecyclerView
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.net.URLEncoder
+
+class JSonDownloader(var context: Context,private var phpUrl:String,var rv:RecyclerView) : AsyncTask<Void, Void, String>() {
+    private val user_id = 4;
+    private lateinit var pd: ProgressDialog
+
+    private fun connect(phpUrl:String):Any{
+        try{
+            val url = URL(phpUrl)
+            val con = url.openConnection() as HttpURLConnection
+
+            con.requestMethod = "POST"
+            con.connectTimeout = 10000
+            con.readTimeout = 10000
+            con.doInput = true
+            con.doOutput = true
+
+            return con
+        }catch (e:MalformedURLException){
+            e.printStackTrace()
+            return "URL ERROR" + e.message
+        }catch (e:IOException){
+            e.printStackTrace()
+            return "CONNECT ERROR" + e.message
+        }
+    }
+
+    private fun download():String{
+        val connection = connect(phpUrl)
+        if(connection.toString().startsWith("Error")){
+            return connection.toString()
+        }
+
+        try{
+            val con = connection as HttpURLConnection
+            val ops = con.outputStream
+            val writer = BufferedWriter(OutputStreamWriter(ops,"UTF-8"))
+            // Send Data to Outsite
+            var data: String = URLEncoder.encode("user_id", "UTF-8") + "=" + URLEncoder.encode(user_id.toString(), "UTF-8")
+            writer.write(data)
+            writer.flush()
+            writer.close()
+            ops.close()
+
+            if(con.responseCode == 200){
+                val `is` = BufferedInputStream(con.inputStream)
+                val br = BufferedReader(InputStreamReader(`is`))
+                val jsonData = StringBuffer()
+                var line:String?
+
+                do{
+                    line = br.readLine()
+                    if(line == null){
+                        break
+                    }
+
+                    //println("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj-> $line")
+                    jsonData.append(line+"\n")
+                }while(true)
+
+                br.close()
+                `is`.close()
+
+                println("ffffffffffffffffffffffffffffffff-> $jsonData")
+
+                return jsonData.toString()
+            }else{
+                return "Error "+con.responseMessage
+            }
+        }catch (e:IOException){
+            return "Error "+e.message
+        }
+    }
+
+    override fun onPostExecute(jsonData: String) {
+        super.onPostExecute(jsonData)
+        pd.dismiss()
+        if(jsonData!!.startsWith("URL ERROR")){
+            val error = jsonData
+            Toast.makeText(context,error,Toast.LENGTH_SHORT).show()
+        }else if(jsonData.startsWith("connect error")){
+            val error = jsonData
+            Toast.makeText(context,error,Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(context,"connection and download success,Now attemt to parse....",Toast.LENGTH_SHORT).show()
+            JsonParser(context,jsonData,rv.execute()
+        }
+    }
+
+    override fun doInBackground(vararg params: Void): String {
+
+        return download()
+    }
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        pd = ProgressDialog(context)
+        pd.setTitle("Download json")
+        pd.setMessage("Message downloading Please wait")
+        pd.show()
+    }
+}
---
+++ MainChat/app/src/main/java/main_chat_application/mainchat/JSonParser.kt
@@ -0,0 +1,148 @@
+package main_chat_application.mainchat
+
+import android.app.ProgressDialog
+import android.content.Context
+import android.os.AsyncTask
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import com
+import org.json.JSONArray
+import org.json.JSONObject
+
+..mainchat.R
+import com.deknerdvariety.prayat.schedule.R.id.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
+@Suppress("DEPRECATION")
+class JsonParser(var context: Context, private var jsonData: String,var rv:RecyclerView) : AsyncTask<Void, Void, Boolean>() {
+
+
+    private lateinit var pd: ProgressDialog
+    //private var courseScheduleL = ArrayList<CourseScheduleSelect>()
+    //private val headScheduleList = ArrayList<HeadScedule>()
+
+//    private fun cal_Time(time_teach:String):ArrayList<String>{
+//        val list = ArrayList<String>()
+//        list.clear()
+//        var startTime = ""
+//        for(i in 0..4){
+//            startTime += time_teach[i]
+//        }
+//        var stopTime = ""
+//        for(i in 6..10){
+//            stopTime += time_teach[i]
+//        }
+//        list.add(startTime)
+//        list.add(stopTime)
+//
+//        return list
+//    }
+
+//    private fun cal_date(date_teach:String):ArrayList<String>{
+//        val list = ArrayList<String>()
+//        list.clear()
+//        var firstDay = ""
+//        if(date_teach[0].toLowerCase().equals('m'))
+//            firstDay = "Monday"
+//        else if(date_teach[0].toLowerCase().equals('t'))
+//            firstDay = "Tuesday"
+//        else if(date_teach[0].toLowerCase().equals('w'))
+//            firstDay = "Wednesday"
+//
+//        var secondDay =""
+//        if(date_teach[date_teach.lastIndex].toLowerCase().equals('f'))
+//            secondDay = "Friday"
+//        else if(date_teach[date_teach.lastIndex].toLowerCase().equals('h'))
+//            secondDay = "Thursday"
+//
+//        list.add(firstDay)
+//        list.add(secondDay)
+//
+//        return list
+//    }
+
+
+    private fun parse(): Boolean {
+        try {
+            val ja = JSONArray(jsonData)
+            var jo = JSONObject()
+
+//            for (i in 0 until ja.length()) {
+//                jo = ja.getJSONObject(i)
+//                if(flagHead == 1){
+//                    val tc_id = jo.getInt("tc_id")
+//                    val semester = jo.getString("semester")
+//
+//                    var h = HeadScedule(tc_id,semester)
+//                    headScheduleList.add(h)
+//                }
+//                else{
+//                    val idCourse = jo.getString("idCourse")
+//                    val time_teach = jo.getString("time_teach")
+//                    val date_teach = jo.getString("date_teach")
+//                    val semester = jo.getString("semester")
+//
+//                    var startTime = cal_Time(time_teach)[0]
+//                    var stopTime = cal_Time(time_teach)[1]
+//
+//                    var firstDay = cal_date(date_teach)[0]
+//                    var secondDay = cal_date(date_teach)[1]
+//
+//                    var c = CourseScheduleSelect(semester,idCourse,startTime,stopTime,firstDay,secondDay)
+//                    courseScheduleL.add(c)
+
+                    //println("$idCourse  $startTime  $stopTime  $firstDay  $secondDay $semester")
+                }
+
+            }
+
+            return true
+        } catch (e: JSONException) {
+            e.printStackTrace()
+            return false
+        }
+    }
+
+
+    override fun onPostExecute(result: Boolean?) {
+        super.onPostExecute(result)
+        pd.dismiss()
+        if (result!!) {
+            //Toast.makeText(context,"adapter pass",Toast.LENGTH_SHORT).show()
+            //var view = LayoutInflater.from(context).inflate(R.layout.downloadphp,viewGroup,false)
+            Teach_schedule_fragment.course_schedule_Head = headScheduleList
+            rv.adapter = TeachHeadingAdapter(headScheduleList,context)
+            // Toast.makeText(context,"ddddddddddddddddddddddddddddddddd",Toast.LENGTH_SHORT).show()
+
+        } else {
+            Toast.makeText(context, "this is data that we try to parse ${jsonData}", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun doInBackground(vararg params: Void?): Boolean {
+        return parse()
+    }
+    override fun onPreExecute() {
+        super.onPreExecute()
+        pd = ProgressDialog(context)
+        pd.setTitle("Parse Json")
+        pd.setMessage("Parsing...Please wait")
+        pd.show()
+    }
+}
+class CourseScheduleSelect(val semester: String?, val idCourse: String?, val startTime: String?, val stopTime: String?, val firstDay: String?, val secondDay: String?) {}
+class HeadScedule(val tc_id:Int,val semester:String)
