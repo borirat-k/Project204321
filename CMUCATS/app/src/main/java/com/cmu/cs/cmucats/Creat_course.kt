de3ebf2f223fba43185e1a333caa0a5c48061369
Rungpailin Disanuwat <rungpailin_disanuwat@cmu.ac.th>
2018-11-18 09:39:21 +0700

    ManageStu_Teacher

---
+++ CMUCATS/app/src/main/java/com/cmu/cs/cmucats/Creat_course.kt
@@ -0,0 +1,12 @@
+package com.cmu.cs.cmucats
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Creat_course : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creat_course)
+    }
+}
---
+++ CMUCATS/app/src/main/java/com/cmu/cs/cmucats/StudentInfo.kt
@@ -0,0 +1,12 @@
+package com.cmu.cs.cmucats
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_info)
+    }
+}
